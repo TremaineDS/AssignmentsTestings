52140b894ad7ac286b337e14b9a2b14274063573
Tremaine Shelton <93730371+TremaineDS@users.noreply.github.com>
2023-12-15 19:33:03 -0500

    Add files via upload

---
+++ DBUtil.java
@@ -0,0 +1,19 @@
+package workassignment;
+
+
+	
+
+	
+
+import java.sql.Connection;
+
+public class DBUtil {
+
+	public static Connection getDBConnection() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
+
+
