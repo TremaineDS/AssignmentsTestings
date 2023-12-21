1bd3284cef7dc82f2b7a555f8f24a2b445d4c9d1
Tremaine Shelton <93730371+TremaineDS@users.noreply.github.com>
2023-12-21 13:48:14 -0500

    Add files via upload

---
+++ DigitsandNonDigits.java
@@ -0,0 +1,19 @@
+package workassignment;
+
+public class DigitsandNonDigits {
+
+	public static void main(String[] args) {
+
+			String sInput = "Page Not Found - 404";
+
+
+			System.out.println(sInput.replaceAll("[^0-5]", ""));
+
+
+
+			}
+
+		
+	}
+
+
